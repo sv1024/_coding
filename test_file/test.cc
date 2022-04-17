4769ca18621563d201510ccf7e10315e1da7bc10
Sergio Pineda <sergio.pineda@nordstorm.com>
2022-04-17 00:03:29 -0500

    working on permutations problem

--- test_file/test.cc
+++ test_file/test.cc
@@ -1,23 +1,44 @@
+#include <cstdlib>
 #include <iostream>
+#include <locale>
+#include <random>
 #include <string>
 #include <vector>
 
 using namespace std;
 int main() {
-  long long n = 0;
+  long long n;
+  bool is_permutable = true;
   cin >> n;
-  vector<long long> nums_vec;
+  vector<long long> permutations_vec;
   vector<long long> sorted_nums;
-  for (long long i = 0; i < n; i++) {
-    nums_vec.push_back(i + 1);
-  }
-  sorted_nums.push_back(nums_vec[0]);
-  nums_vec.pop_back();
-  cout << nums_vec.size();
-  for (int i = 0; i < n; i++) {
-  };
 
+  for (long long i = 0; i < n; i++) {
+    sorted_nums.push_back(i + 1);
+  }
+
+  permutations_vec.push_back(sorted_nums[0]);
+  sorted_nums.erase(sorted_nums.begin());
+  while (is_permutable) {
+
+    for (long long i = 0; i < sorted_nums.size(); i++) {
+      if (abs(permutations_vec[permutations_vec.size() - 1] - sorted_nums[i]) !=
+          1) {
+        permutations_vec.push_back(sorted_nums[i]);
+        sorted_nums.erase(sorted_nums.begin() + i);
+        i = sorted_nums.size();
+      } else {
+        if (i == sorted_nums.size() - 1) {
+          is_permutable = false;
+        }
+      }
+    }
+  }
+  for (long long i = 0; i < permutations_vec.size(); i++) {
+    cout << permutations_vec[i] << " ";
+  }
   return 0;
 }
+
 // https://cses.fi/problemset/task/1070
 //*(&nums_arr + 1) - nums_arr
